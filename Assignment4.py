21ffeb8c3cf1ad98d5ab7c84b9d853db1c00b03b
= <epautsch@luc.edu>
2023-03-17 00:53:14 -0500

    Initial commit

---
+++ Assignment4.py
@@ -0,0 +1,161 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, random_split
+
+
+class ResNetBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(ResNetBlock, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10, dropout_rate=0.5):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self.make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self.make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self.make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self.make_layer(block, 512, num_blocks[3], stride=2)
+        self.dropout = nn.Dropout(dropout_rate)
+        self.linear = nn.Linear(512, num_classes)
+
+    def make_layer(self, block, out_channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.dropout(out)
+        out = self.linear(out)
+        return out
+
+
+transform = transforms.Compose(
+    [transforms.RandomHorizontalFlip(),
+     transforms.RandomCrop(32, padding=4),
+     transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+)
+
+data_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+train_size = int(0.7 * len(data_set))
+test_size = len(data_set) - train_size
+train_set, test_set = random_split(data_set, [train_size, test_size])
+
+train_loader = DataLoader(train_set, batch_size=32, shuffle=True, num_workers=2)
+test_loader = DataLoader(test_set, batch_size=32, shuffle=False, num_workers=2)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+net = ResNet(ResNetBlock, [3, 4, 6, 3], dropout_rate=0.5).to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(net.parameters(), lr=1e-4)
+
+
+def train(net, data_loader, criterion, optimizer, device):
+    net.train()
+    running_loss = 0.0
+    for i, data in enumerate(data_loader, 0):
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+    return running_loss / len(data_loader)
+
+
+def test(net, data_loader, device):
+    net.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in data_loader:
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    return 100 * correct / total
+
+
+if __name__ == '__main__':
+    num_epochs = 50
+    for epoch in range(num_epochs):
+        train_loss = train(net, train_loader, criterion, optimizer, device)
+        print(f'Epoch: {epoch + 1}, Loss: {train_loss:.4f}')
+
+    accuracy = test(net, test_loader, device)
+    print(f'Accuracy: {accuracy:.2f}%')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
